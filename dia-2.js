a8f09d251445511fe27a0e7ca10c07cf9ad806e8
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-04-13 19:25:42 -0400

    feat: Add dia 2

---
+++ dia-2.js
@@ -0,0 +1,4 @@
+let nome = prompt("- Qual é seu nome ?");
+let idade = prompt("- Qual a sua idade ?");
+let linguagem = prompt("- Qual linguagem de programação está estudando ?");
+console.log(`Olá ${nome}, você tem ${idade} de idade e já está aprendendo ${linguagem}`);
